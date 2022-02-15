a3acd4433ecc1a0f69e1b5afc8207d230a71de80
hustlerabhay <97334718+hustlerabhay@users.noreply.github.com>
2022-02-16 02:44:36 +0530

    Add files via upload

---
+++ modules/config.py
@@ -0,0 +1,18 @@
+import os
+import aiohttp
+from os import getenv
+from dotenv import load_dotenv
+    
+load_dotenv()
+que = {}
+admins = {}
+
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+BOT_TOKEN = getenv("BOT_TOKEN")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "300"))
+STRING_SESSION = getenv("STRING_SESSION")
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1282754256").split()))
+ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/be4d41c7d82b35b04cb1a.jpg")
+aiohttpsession = aiohttp.ClientSession()
