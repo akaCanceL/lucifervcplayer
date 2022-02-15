ff20e1cf65f0bdb6e74ae634217a3a58b4a29f24
hustlerabhay <97334718+hustlerabhay@users.noreply.github.com>
2022-02-16 02:58:19 +0530

    Update alive.py

--- plugins/alive.py
+++ plugins/alive.py
@@ -59,7 +59,7 @@ async def start_(client: Client, message: Message):
     )
     
     
-@Client.on_message(commandpro(["/start", "/alive", "uranium"]) & filters.group & ~filters.edited)
+@Client.on_message(commandpro(["/start", "/alive", "lucifermusic"]) & filters.group & ~filters.edited)
 async def start(client: Client, message: Message):
     await message.reply_photo(
         photo=f"{ALIVE_IMG}",
